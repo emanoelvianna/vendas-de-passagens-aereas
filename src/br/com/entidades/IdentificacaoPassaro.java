15e042b913457af2582d265fd78343ce672e11c3
emanoelvianna <emanoel.vianna@outlook.com>
2016-05-22 15:22:47 -0300

    pequena modificação na class

--- src/br/com/entidades/IdentificacaoPassaro.java
+++ src/br/com/entidades/IdentificacaoPassaro.java
@@ -7,6 +7,7 @@ public class IdentificacaoPassaro {
 	private String nome;
 	private Documento documento;
 	private String numeroDocumento;
+	private Integer numeroLocalizador;
 
 	public IdentificacaoPassaro(String nome, Documento documento, String numeroDocumento) {
 		this.nome = nome;
@@ -24,5 +25,9 @@ public class IdentificacaoPassaro {
 
 	public String getNumeroDocumento() {
 		return this.numeroDocumento;
+	}
+
+	public Integer getNumeroLocalizador() {
+		return numeroLocalizador;
 	}
 }
